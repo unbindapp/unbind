08d4c64964cc57ca642710850b7502690260816c
yekta <ngyekta@gmail.com>
2025-02-17 13:04:20 +0300

    Padding tweak

--- src/components/bottom-drawer.tsx
+++ src/components/bottom-drawer.tsx
@@ -39,9 +39,9 @@ export default function BottomDrawer({
         hasHandle
       >
         <div className="w-full flex items-center justify-start gap-1 border-b">
-          <DrawerHeader className="flex-1 min-w-0 flex items-center justify-start p-0">
-            {Icon && <Icon className="size-7 sm:size-8 -ml-1" />}
-            <DrawerTitle className="pl-5 min-w-0 gap-2.5 py-3.5 whitespace-nowrap overflow-hidden overflow-ellipsis shrink leading-tight text-xl sm:text-2xl text-left">
+          <DrawerHeader className="flex-1 px-0 py-3 min-w-0 flex items-center justify-start">
+            {Icon && <Icon className="size-7 sm:size-8 -ml-1 -my-2" />}
+            <DrawerTitle className="pl-5 min-w-0 gap-2.5 whitespace-nowrap overflow-hidden overflow-ellipsis shrink leading-tight text-xl sm:text-2xl text-left">
               {title}
             </DrawerTitle>
           </DrawerHeader>
