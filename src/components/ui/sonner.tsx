cccf06bcd3fae5ae70940c0e03db256023c7a92d
yekta <ngyekta@gmail.com>
2025-03-03 10:04:58 +0300

    Sonner color fixes

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -28,7 +28,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
           toast:
             "bg-background border border-border rounded-xl px-5 data-type/toast:px-4.5 py-3.5 flex flex-row items-center gap-1.5",
           content: "shrink min-w-0 flex flex-col gap-1",
-          description: "text-muted-foreground text-sm leading-snug",
+          description:
+            "group-data-[type=error]/toast:text-foreground! group-data-[type=warning]/toast:text-foreground! group-data-[type=success]/toast:text-foreground! text-muted-foreground! text-sm leading-snug",
           closeButton:
             "size-6 p-1 has-hover:hover:text-foreground! active:text-foreground! text-muted-foreground! shadow-md shadow-shadow/shadow border border-border before:w-full before:h-full before:min-w-[48px] before:min-h-[48px] before:z-[-1] z-10 before:bg-transparent before:absolute border-border! active:bg-border! has-hover:hover:bg-border! bg-background! left-1 top-1",
           actionButton:
