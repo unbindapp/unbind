adb8ef7a63b6f59d93ab81c98cc0364fcc6f6f17
Brandon Berhent <bbedward@gmail.com>
2025-03-04 12:43:18 -0500

    Refresh token

--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,5 +1,6 @@
 import { env } from "@/lib/env";
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession, Account } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import { cache } from "react";
 
 declare module "next-auth" {
@@ -7,9 +8,67 @@ declare module "next-auth" {
     user: {
       email: string;
     } & DefaultSession["user"];
-    accessToken: string;
+    access_token: string;
   }
 }
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token: string
+    expires_at: number
+    refresh_token?: string
+    error?: "RefreshTokenError"
+  }
+}
+
+interface TokenRefreshResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
+  id_token: string;
+}
+
+async function refreshAccessToken(token: JWT) {
+  try {
+    console.log("Refreshing access token...", token);
+    const url = `${env.DEX_ISSUER}/token`; 
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        client_id: env.DEX_CLIENT_ID,
+        client_secret: env.DEX_CLIENT_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: token.refresh_token || "",
+      }),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw errorResponse
+    }
+
+    const refreshedTokens = await response.json() as TokenRefreshResponse ;
+
+    const expiresAt = Date.now() + (refreshedTokens.expires_in * 1000);
+
+    return {
+      ...token,
+      access_token: refreshedTokens.access_token,
+      expires_at: expiresAt,
+      refresh_token: refreshedTokens.refresh_token ?? token.refresh_token,
+      iat: Date.now() / 1000,
+    };
+  } catch (error) {
+    console.error("Error refreshing access token:", error);
+    return {
+      ...token,
+      error: "RefreshTokenError",
+    };
+  }
+}
+
 
 const {
   handlers,
@@ -19,15 +78,32 @@ const {
 } = NextAuth({
   callbacks: {
     jwt: async ({ token, account }) => {
+      // First sign in
       if (account) {
-        token.accessToken = account?.access_token;
+        return {
+          ...token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          refresh_token: account.refresh_token,
+          iat: Date.now() / 1000,
+        } as JWT;
       }
-      return token;
+
+      console.log("JWT", token);
+
+      // Token not expired, and has more than 10% life left
+      if (token.iat && Date.now() < (token.iat + (token.expires_at - token.iat) * 0.9) * 1000) {
+        return token;
+      }
+
+       const r = await refreshAccessToken(token) as JWT;
+       console.log("Refreshed token", r);
+       return r;
     },
     session: async ({ session, token }) => {
       return {
         ...session,
-        accessToken: token.accessToken,
+        accessToken: token.access_token,
         user: session.user,
       };
     },
@@ -40,6 +116,9 @@ const {
       issuer: env.DEX_ISSUER,
       clientId: env.DEX_CLIENT_ID,
       clientSecret: env.DEX_CLIENT_SECRET,
+      authorization: { params: { scope: "openid email profile groups offline_access" } },
+      checks: ["pkce", "state"],
+      wellKnown: `${env.DEX_ISSUER}/.well-known/openid-configuration`,
     },
   ],
   pages: {
