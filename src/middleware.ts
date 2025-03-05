5dd0ed51ec387ee9ca249ae23bb888cbaaeb9e87
yekta <ngyekta@gmail.com>
2025-03-05 07:57:30 +0300

    Middleware tweaks

---
+++ src/middleware.ts
@@ -0,0 +1,193 @@
+import { encode, getToken, JWT } from "next-auth/jwt";
+import { NextMiddleware, NextRequest, NextResponse } from "next/server";
+import { jwtDecode } from "jwt-decode";
+import { z } from "zod";
+
+const isRefreshing = new Map<string, boolean>();
+const signInPathname = "/sign-in";
+
+const sessionCookieName =
+  process.env.NODE_ENV === "production" ? "__Secure-authjs.session-token" : "authjs.session-token";
+const sessionCookieMaxAge = 60 * 60 * 24 * 14;
+const secureCookie = process.env.NODE_ENV === "production";
+const sameSiteCookie = process.env.NODE_ENV === "production" ? "strict" : "lax";
+
+const tokenExpiry = 60;
+const tokenRefreshBuffer = 30;
+
+const authSecret = process.env.AUTH_SECRET!;
+const dexClientId = process.env.DEX_CLIENT_ID!;
+const dexClientSecret = process.env.DEX_CLIENT_SECRET!;
+const dexIssuer = process.env.DEX_ISSUER!;
+
+if (!authSecret) throw new Error("AUTH_SECRET is not set");
+if (!dexClientId) throw new Error("DEX_CLIENT_ID is not set");
+if (!dexClientSecret) throw new Error("DEX_CLIENT_SECRET is not set");
+if (!dexIssuer) throw new Error("DEX_ISSUER is not set");
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder files (images, fonts, etc.)
+     */
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|gif|png|svg|ico|css|js)).*)",
+  ],
+};
+
+export const middleware: NextMiddleware = async (request: NextRequest) => {
+  console.log("🔀 Middleware");
+  const token = await getToken({
+    cookieName: sessionCookieName,
+    req: request,
+    secret: process.env.AUTH_SECRET,
+    secureCookie,
+  });
+
+  let response = NextResponse.next();
+  const signInUrl = new URL(signInPathname, request.url);
+  const homeUrl = new URL("/", request.url);
+
+  if (!token) {
+    if (request.nextUrl.pathname === signInPathname) {
+      return response;
+    }
+    return NextResponse.redirect(signInUrl);
+  }
+
+  if (token && request.nextUrl.pathname === signInPathname) {
+    return NextResponse.redirect(homeUrl);
+  }
+
+  if (shouldUpdateToken(token)) {
+    try {
+      const newTokensObject = await refreshAccessToken(token);
+      const sessionCookieValue = await encode({
+        secret: authSecret,
+        token: newTokensObject,
+        maxAge: tokenExpiry,
+        salt: sessionCookieName,
+      });
+      response = updateCookie(sessionCookieValue, request, response, signInUrl);
+    } catch (error) {
+      console.log("Error refreshing token: ", error);
+      return updateCookie(null, request, response, signInUrl);
+    }
+  }
+
+  return response;
+};
+
+export function updateCookie(
+  sessionCookieValue: string | null,
+  request: NextRequest,
+  response: NextResponse,
+  signInUrl: URL,
+): NextResponse<unknown> {
+  if (sessionCookieValue) {
+    request.cookies.set(sessionCookieName, sessionCookieValue);
+    response = NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+    response.cookies.set(sessionCookieName, sessionCookieValue, {
+      httpOnly: true,
+      maxAge: sessionCookieMaxAge,
+      secure: secureCookie,
+      sameSite: sameSiteCookie,
+    });
+  } else {
+    request.cookies.delete(sessionCookieName);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return response;
+}
+
+export function shouldUpdateToken(token: JWT): boolean {
+  const expiresAt = token?.expires_at;
+  if (!expiresAt) {
+    return true;
+  }
+  const timeInSeconds = Math.ceil(Date.now() / 1000);
+  const shouldUpdate = timeInSeconds >= expiresAt - tokenRefreshBuffer;
+  return shouldUpdate;
+}
+
+export async function refreshAccessToken(token: JWT): Promise<JWT> {
+  const refreshToken = token?.refresh_token;
+  if (!refreshToken) {
+    throw new Error("No refresh token found in token");
+  }
+
+  console.log("Refreshing token");
+  if (isRefreshing.get(refreshToken)) {
+    console.log("Token is already being refreshed");
+    return token;
+  }
+
+  isRefreshing.set(refreshToken, true);
+
+  try {
+    const url = `${dexIssuer}/token`;
+    const res = await fetch(url, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        client_id: dexClientId,
+        client_secret: dexClientSecret,
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+      }),
+
+      credentials: "include",
+      method: "POST",
+    });
+
+    const resJson = await res.json();
+
+    if (!res.ok) {
+      console.log("Res is not ok:", resJson);
+      throw new Error(`Token refresh failed with status: ${res.status}`);
+    }
+
+    const newTokensObj = TokenRefreshResponseSchema.parse(resJson);
+    const decodedToken = jwtDecode(newTokensObj.access_token);
+    const newJwt: JWT = {
+      ...token,
+      exp: decodedToken.exp,
+      iat: decodedToken.iat,
+      access_token: newTokensObj.access_token ?? token?.access_token,
+      expires_at: decodedToken.exp,
+      refresh_token: newTokensObj.refresh_token ?? token?.refresh_token,
+    };
+    return newJwt;
+  } catch (e) {
+    console.error("refreshAccessToken error:", e);
+  } finally {
+    isRefreshing.delete(refreshToken);
+  }
+
+  return token;
+}
+
+const TokenRefreshResponseSchema = z.object({
+  access_token: z.string(),
+  token_type: z.string(),
+  expires_in: z.number(),
+  refresh_token: z.string(),
+  id_token: z.string(),
+});
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token?: string;
+    expires_at?: number;
+    refresh_token?: string;
+    error?: "RefreshTokenError";
+  }
+}
--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -4,8 +4,6 @@ import { JWT } from "next-auth/jwt";
 import { cache } from "react";
 import { z } from "zod";
 
-const refreshRatio = 0.9;
-
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
@@ -23,37 +21,17 @@ const {
 } = NextAuth({
   callbacks: {
     jwt: async ({ token, account }) => {
-      console.log("JWT callback");
-
       if (account) {
-        console.log("First sign in");
         const newToken: JWT = {
           ...token,
           access_token: account.access_token,
           expires_at: account.expires_at,
           refresh_token: account.refresh_token,
-          iat: Date.now() / 1000,
         };
         return newToken;
       }
 
-      const originalExpiresInMs =
-        token.expires_at && token.iat ? (token.expires_at - token.iat) * 1000 : undefined;
-      const sinceIssuedMs = token.iat ? Date.now() - token.iat * 1000 : undefined;
-
-      const shouldNotUpdate =
-        originalExpiresInMs !== undefined &&
-        sinceIssuedMs !== undefined &&
-        sinceIssuedMs < originalExpiresInMs * refreshRatio;
-
-      if (shouldNotUpdate) {
-        const expiresIn = originalExpiresInMs - sinceIssuedMs;
-        console.log(`Token not expired, expires in: ${expiresIn}ms`);
-        return token;
-      }
-
-      const res = await refreshTokens(token);
-      return res;
+      return token;
     },
     session: async ({ session, token }) => {
       return {
@@ -85,71 +63,3 @@ const {
 const auth = cache(uncachedAuth);
 
 export { auth, handlers, signIn, signOut };
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    access_token?: string;
-    expires_at?: number;
-    refresh_token?: string;
-    error?: "RefreshTokenError";
-  }
-}
-
-const TokenRefreshResponseSchema = z.object({
-  access_token: z.string(),
-  token_type: z.string(),
-  expires_in: z.number(),
-  refresh_token: z.string(),
-  id_token: z.string(),
-});
-
-async function refreshTokens(token: JWT): Promise<JWT> {
-  try {
-    console.log("Refreshing access token...");
-    if (!token.refresh_token) {
-      throw new Error("No refresh token provided");
-    }
-
-    const url = `${env.DEX_ISSUER}/token`;
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams({
-        client_id: env.DEX_CLIENT_ID,
-        client_secret: env.DEX_CLIENT_SECRET,
-        grant_type: "refresh_token",
-        refresh_token: token.refresh_token,
-      }),
-    });
-
-    if (!response.ok) {
-      const errorResponse = await response.json();
-      throw errorResponse;
-    }
-
-    const res = await response.json();
-    const resParsed = TokenRefreshResponseSchema.parse(res);
-
-    console.log("Refreshed tokens");
-
-    const expiresAt = Date.now() + resParsed.expires_in * 1000;
-
-    const newToken: JWT = {
-      ...token,
-      access_token: resParsed.access_token ?? token.access_token,
-      expires_at: expiresAt,
-      refresh_token: resParsed.refresh_token ?? token.refresh_token,
-      iat: Math.round(Date.now() / 1000),
-      exp: Math.round(expiresAt / 1000),
-    };
-
-    return newToken;
-  } catch (error) {
-    console.error("Error refreshing access token:", error);
-    return {
-      ...token,
-      error: "RefreshTokenError",
-    };
-  }
-}
