62557fb96cf044e8c75897ef57c537decc56a69a
yekta <ngyekta@gmail.com>
2025-04-13 13:48:04 +0300

    Back to normal label

--- src/app/(project)/[team_id]/project/[project_id]/settings/webhooks/_components/add-webhook-form.tsx
+++ src/app/(project)/[team_id]/project/[project_id]/settings/webhooks/_components/add-webhook-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ErrorLine from "@/components/error-line";
+import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/components/ui/utils";
 import { useAppForm } from "@/lib/hooks/use-app-form";
 import { z } from "zod";
@@ -94,18 +95,31 @@ export default function AddWebhookForm({ className }: TProps) {
                 </h3>
                 <div className="-mx-3 mt-1.5 flex w-[calc(100%+1.5rem)] flex-col items-start justify-start">
                   {group.options.map((option) => (
-                    <div
+                    <form.AppField
                       key={option.id}
-                      className="has-hover:hover:bg-border active:bg-border flex w-full cursor-pointer items-center gap-2.5 rounded-md"
-                    >
-                      <input type="checkbox" name={option.id} id={option.id} className="-ml-0.25" />
-                      <label
-                        htmlFor={option.id}
-                        className="min-w-0 flex-1 shrink py-2.5 pr-3.5 leading-tight select-none"
-                      >
-                        {option.title}
-                      </label>
-                    </div>
+                      name="selectedIds"
+                      children={(field) => (
+                        <label
+                          key={option.id}
+                          className="has-hover:hover:bg-border active:bg-border flex w-full cursor-pointer items-center gap-2.5 rounded-md px-3.5 py-2.5"
+                        >
+                          <Checkbox
+                            onBlur={field.handleBlur}
+                            checked={field.state.value.has(option.id)}
+                            onCheckedChange={(c) => {
+                              field.handleChange((prev) => {
+                                const newSet = new Set(prev);
+                                if (c) newSet.add(option.id);
+                                else newSet.delete(option.id);
+                                return newSet;
+                              });
+                            }}
+                            className="-ml-0.25"
+                          />
+                          <p className="min-w-0 shrink leading-tight select-none">{option.title}</p>
+                        </label>
+                      )}
+                    />
                   ))}
                 </div>
               </div>
