686ab30e372aed0d7fa4f388bdb49ff07d767083
yekta <ngyekta@gmail.com>
2025-04-13 13:30:28 +0300

    Remove html fot

--- src/app/(project)/[team_id]/project/[project_id]/settings/webhooks/_components/add-webhook-form.tsx
+++ src/app/(project)/[team_id]/project/[project_id]/settings/webhooks/_components/add-webhook-form.tsx
@@ -95,7 +95,6 @@ export default function AddWebhookForm({ className }: TProps) {
                 <div className="-mx-3 mt-1.5 flex w-[calc(100%+1.5rem)] flex-col items-start justify-start">
                   {group.options.map((option) => (
                     <label
-                      htmlFor={option.id}
                       key={option.id}
                       className="has-hover:hover:bg-border active:bg-border flex w-full cursor-pointer items-center gap-2.5 rounded-md px-3.5 py-2.5"
                     >
