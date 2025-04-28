89bb6453f0e771ab8a884428b23b01ce62a68d65
yekta <ngyekta@gmail.com>
2025-04-28 10:43:29 +0300

    Better domain card

--- src/components/service/panel/content/undeployed/domain-card.tsx
+++ src/components/service/panel/content/undeployed/domain-card.tsx
@@ -43,42 +43,45 @@ export function DomainCard({ domain, className }: { domain: string; className?: 
 
   return (
     <div
+      data-configured={dnsCheckData?.data.dns_configured ? true : undefined}
       data-pending={!data && isPending ? true : undefined}
       data-error={!data && !isPending && error ? true : undefined}
       className={cn(
-        "group/card z-0 flex w-full flex-col items-start justify-start gap-1 rounded-lg border text-sm select-text",
+        "group/card data-configured:bg-success/6 data-configured:border-success/10 z-0 flex w-full flex-col items-start justify-start rounded-lg border text-sm select-text",
         className,
       )}
     >
       {(!data || !dnsCheckData?.data.dns_configured) && (
-        <div className="flex w-full items-start justify-start px-3 pt-2 pb-2.5">
-          <div className="flex max-w-1/3 flex-col gap-0.5 pr-6">
-            <p className="text-muted-foreground leading-tight">Type</p>
-            <p className="leading-tight font-medium">A</p>
-          </div>
-          <div className="flex min-w-0 shrink flex-col gap-0.5 pr-6">
-            <p className="text-muted-foreground leading-tight">Name</p>
-            <p className="leading-tight font-medium">{domain}</p>
-          </div>
-          <div className="flex max-w-1/3 flex-col gap-0.5">
-            <p className="text-muted-foreground leading-tight">Content</p>
-            <p className="group-data-error/card:text-destructive group-data-pending/card:animate-skeleton group-data-pending/card:bg-foreground leading-tight font-medium group-data-pending/card:rounded-md group-data-pending/card:text-transparent">
-              {data
-                ? data?.data.external_ipv4 || data?.data.external_ipv6
-                : error
-                  ? "Error"
-                  : "Loading..."}
-            </p>
+        <div className="flex w-full flex-col items-start justify-start">
+          <p className="w-full px-3 py-2.5 leading-tight font-medium">
+            Create the DNS record below:
+          </p>
+          <div className="flex w-full items-start justify-start border-t border-b px-3 pt-2 pb-2.5">
+            <div className="flex max-w-1/3 flex-col gap-0.5 pr-6">
+              <p className="text-muted-foreground leading-tight">Type</p>
+              <p className="leading-tight font-medium">A</p>
+            </div>
+            <div className="flex min-w-0 shrink flex-col gap-0.5 pr-6">
+              <p className="text-muted-foreground leading-tight">Name</p>
+              <p className="leading-tight font-medium">{domain}</p>
+            </div>
+            <div className="flex max-w-1/3 flex-col gap-0.5">
+              <p className="text-muted-foreground leading-tight">Content</p>
+              <p className="group-data-error/card:text-destructive group-data-pending/card:animate-skeleton group-data-pending/card:bg-foreground leading-tight font-medium group-data-pending/card:rounded-md group-data-pending/card:text-transparent">
+                {data
+                  ? data?.data.external_ipv4 || data?.data.external_ipv6
+                  : error
+                    ? "Error"
+                    : "Loading..."}
+              </p>
+            </div>
           </div>
         </div>
       )}
       {data && (
-        <div
-          data-configured={dnsCheckData?.data.dns_configured ? true : undefined}
-          className="data-configured:text-success text-muted-foreground -mt-3 flex flex-row flex-wrap gap-1.5 px-3 py-3 data-configured:mt-0"
-        >
+        <div className="group-data-configured/card:text-success text-muted-foreground flex flex-row flex-wrap gap-1.5 px-3 py-2.5 group-data-configured/card:mt-0">
           <div className="flex max-w-full items-center justify-start gap-1.5 pr-4">
-            <div className="-ml-0.25 size-4 shrink-0">
+            <div className="-ml-0.25 size-3.5 shrink-0">
               {dnsCheckData?.data.dns_configured ? (
                 <CheckCircleIcon className="size-full" />
               ) : (
@@ -86,12 +89,12 @@ export function DomainCard({ domain, className }: { domain: string; className?: 
               )}
             </div>
             <p className="min-w-0 shrink leading-tight font-medium">
-              {dnsCheckData?.data.dns_configured ? "DNS detected" : "Waiting for DNS record..."}
+              {dnsCheckData?.data.dns_configured ? "DNS record detected" : "Waiting for DNS record"}
             </p>
           </div>
           {dnsCheckData?.data.cloudflare && (
             <div className="flex max-w-full items-center justify-start gap-1.5 pr-4">
-              <div className="-ml-0.25 size-4 shrink-0">
+              <div className="-ml-0.25 size-3.5 shrink-0">
                 <BrandIcon brand="cloudflare" className="size-full scale-110" />
               </div>
               <p className="min-w-0 shrink leading-tight font-medium">Cloudflare detected</p>
