7ee705905b56c16d49a2055035ca252a8d8bcf31
yekta <ngyekta@gmail.com>
2025-05-03 07:02:57 +0300

    Deployment prebuild errors

--- src/components/deployment/panel/tabs/build-logs/build-logs.tsx
+++ src/components/deployment/panel/tabs/build-logs/build-logs.tsx
@@ -1,6 +1,10 @@
 import { useDeployment } from "@/components/deployment/deployment-provider";
+import ErrorLine from "@/components/error-line";
 import LogViewer from "@/components/logs/log-viewer";
+import TabWrapper from "@/components/navigation/tab-wrapper";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { TDeploymentShallow } from "@/server/trpc/api/deployments/types";
+import { TriangleAlertIcon } from "lucide-react";
 
 type TProps = {
   deployment: TDeploymentShallow;
@@ -16,6 +20,25 @@ export default function BuildLogs({ deployment }: TProps) {
 
   const createdAtTimestamp = createdAt ? new Date(createdAt).getTime() : undefined;
   const completedAtTimestamp = completedAt ? new Date(completedAt).getTime() : undefined;
+
+  if (deployment.error) {
+    return (
+      <ScrollArea>
+        <TabWrapper>
+          <div className="flex w-full flex-col gap-3">
+            <div className="text-destructive flex w-full items-center justify-start gap-2 px-1.5">
+              <TriangleAlertIcon className="size-4 shrink-0" />
+              <h3 className="shrink leading-tight font-semibold">Failed at pre-build stage</h3>
+            </div>
+            <ErrorLine
+              className="border-destructive/8 text-muted-foreground bg-destructive/6 border"
+              message={deployment.error}
+            ></ErrorLine>
+          </div>
+        </TabWrapper>
+      </ScrollArea>
+    );
+  }
 
   return (
     <LogViewer
