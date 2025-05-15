ec62d82a5a3cfb5389380dbe2bc31851a28b23d8
yekta <ngyekta@gmail.com>
2025-05-15 16:52:51 +0300

    Template panel services and inputs

--- src/components/templates/panel/template-draft-panel-content.tsx
+++ src/components/templates/panel/template-draft-panel-content.tsx
@@ -1,10 +1,11 @@
 import ErrorLine from "@/components/error-line";
+import BrandIcon from "@/components/icons/brand";
 import { TTemplateDraft } from "@/components/templates/template-draft-store";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/components/ui/utils";
 import { LoaderIcon } from "lucide-react";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useMemo } from "react";
 
 type TProps = {
   templateDraft: TTemplateDraft;
@@ -14,16 +15,30 @@ type TProps = {
 export default function TemplateDraftPanelContent({ templateDraft, className, ...rest }: TProps) {
   return (
     <div
-      className={cn("mt-4 flex w-full flex-1 flex-col overflow-hidden border-t sm:mt-6", className)}
+      className={cn(
+        "mt-4 flex w-full flex-1 flex-col overflow-hidden border-t select-text sm:mt-6",
+        className,
+      )}
       {...rest}
     >
       <ScrollArea classNameViewport="pb-8">
         <div className="flex w-full flex-1 flex-col gap-6 px-3 py-4 sm:p-6">
-          {templateDraft.template.definition.services.map((service) => (
-            <p key={service.id} className="w-full">
-              {service.name}
-            </p>
-          ))}
+          {/* Inputs */}
+          <div className="flex w-full flex-col gap-2">
+            {templateDraft.template.definition.inputs.map((input) => (
+              <div key={input.id} className="flex w-full flex-col px-1">
+                <p className="w-full font-bold">{input.name}</p>
+                <p className="text-muted-foreground w-full">{input.description}</p>
+              </div>
+            ))}
+          </div>
+          {/* Services */}
+          <div className="flex w-full flex-col gap-2">
+            <h3 className="w-full px-1 pb-1 text-xl leading-tight font-bold">Services</h3>
+            {templateDraft.template.definition.services.map((service) => (
+              <TemplateServiceCard key={service.id} service={service} />
+            ))}
+          </div>
         </div>
       </ScrollArea>
       <div className="flex w-full flex-col gap-2 border-t px-3 pt-3 pb-[calc(var(--safe-area-inset-bottom)+0.75rem)] sm:px-6 sm:pt-6 sm:pb-[calc(var(--safe-area-inset-bottom)+1.5rem)]">
@@ -50,6 +65,35 @@ export default function TemplateDraftPanelContent({ templateDraft, className, ..
             </div>
           </Button>
         </form>
+      </div>
+    </div>
+  );
+}
+
+function TemplateServiceCard({
+  service,
+}: {
+  service: TTemplateDraft["template"]["definition"]["services"][number];
+}) {
+  const descriptionText = useMemo(() => {
+    if (service.type === "docker-image") {
+      return service.image;
+    }
+    if (service.type === "database") {
+      return `${service.database_type}:${service.database_config?.version || "latest"}`;
+    }
+    return "Unknown service type";
+  }, [service]);
+  return (
+    <div className="flex w-full flex-col rounded-xl border px-4 py-3.5">
+      <div className="flex w-full items-center justify-start gap-3">
+        <BrandIcon brand={service.icon} color="brand" className="size-6 shrink-0" />
+        <div className="-mt-0.5 flex min-w-0 flex-1 flex-col justify-start gap-0.5">
+          <p className="min-w-0 shrink leading-tight font-semibold">{service.name}</p>
+          <p className="text-muted-foreground min-w-0 shrink text-sm leading-tight">
+            {descriptionText}
+          </p>
+        </div>
       </div>
     </div>
   );
