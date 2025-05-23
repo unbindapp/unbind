10262206730e955bdb04eb04fbea8c2f51e602e9
yekta <ngyekta@gmail.com>
2025-05-24 01:44:58 +0300

    VolumeLine component improvements

--- src/components/volume/volume-line.tsx
+++ src/components/volume/volume-line.tsx
@@ -25,12 +25,12 @@ export default function VolumeLine({
   className,
 }: TProps) {
   const usagePercentage = useMemo(() => {
-    if (!volume.used_gb || !volume.size_gb) return 0;
+    if (!volume.used_gb || !volume.size_gb) return undefined;
     return Math.min(Math.max(0, Math.ceil((volume.used_gb / volume.size_gb) * 100)), 100);
   }, [volume]);
 
   const usageLevel: TUsageLevel = useMemo(() => {
-    if (usagePercentage >= 85) return "high";
+    if (usagePercentage !== undefined && usagePercentage >= 85) return "high";
     return "low";
   }, [usagePercentage]);
 
@@ -44,13 +44,14 @@ export default function VolumeLine({
       <Button
         variant={"ghost"}
         data-usage={usageLevel}
+        data-no-percentage={usagePercentage === undefined ? true : undefined}
         key={volume.id}
         className={cn(
-          "group/line relative w-full overflow-hidden rounded-none border px-0 py-2",
+          "group/line bg-background relative w-full overflow-hidden rounded-none border px-0 py-2",
           className,
         )}
       >
-        {volume.used_gb && volume.size_gb && (
+        {usagePercentage !== undefined && (
           <div className="absolute top-0 left-0 h-full w-full">
             <div
               style={{
@@ -61,17 +62,17 @@ export default function VolumeLine({
           </div>
         )}
         <div className="text-muted-foreground group-data-[usage=high]/line:text-warning flex w-full items-center justify-between gap-4 px-4">
-          <div className="relative flex min-w-0 shrink items-center gap-1.75 leading-tight font-medium">
-            <div className="flex shrink-0 items-center justify-start gap-1.25">
+          <div className="relative flex w-full items-center justify-between gap-4 leading-tight font-medium">
+            <div className="flex shrink-0 items-center justify-start gap-1.5 group-data-no-percentage/line:gap-2">
               <HardDriveIcon className="size-3.5" />
-              {volume.used_gb && volume.size_gb && <p>{usagePercentage}%</p>}
+              {usagePercentage !== undefined && <p>{usagePercentage}%</p>}
+              {usagePercentage === undefined && (
+                <p className="min-w-0 shrink truncate text-right">Storage {index + 1}</p>
+              )}
             </div>
-            {volume.used_gb && volume.size_gb && (
-              <p className="text-muted-more-foreground group-data-[usage=high]/line:text-warning/35">
-                •
-              </p>
+            {usagePercentage !== undefined && (
+              <p className="min-w-0 shrink truncate text-right">Storage {index + 1}</p>
             )}
-            <p className="min-w-0 shrink truncate">Storage {index + 1}</p>
           </div>
         </div>
       </Button>
