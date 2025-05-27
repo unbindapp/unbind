940344f698f67e2cdc8583909703f42f21cef4b1
yekta <ngyekta@gmail.com>
2025-05-27 08:19:50 +0300

    shrink-0 on hourglass icon

--- src/components/volume/panel/tabs/settings/sections/expand-section.tsx
+++ src/components/volume/panel/tabs/settings/sections/expand-section.tsx
@@ -90,7 +90,7 @@ export default function ExpandSection({ volume }: TProps) {
     return (
       <div className="flex w-full flex-col gap-2 md:max-w-xl">
         <div className="bg-warning/8 border-warning/8 text-warning flex w-full items-start justify-start gap-2 rounded-lg border px-3 py-2.5 font-medium">
-          <HourglassIcon className="animate-hourglass mt-0.5 -ml-0.5 size-4" />
+          <HourglassIcon className="animate-hourglass mt-0.5 -ml-0.5 size-4 shrink-0" />
           <p className="min-w-0 shrink leading-tight">
             Expanding the volume. This could take a couple of minutes.
           </p>
