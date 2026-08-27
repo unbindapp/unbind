81af156f1459b45949fb1cc995c1b68f8ae926c5
yekta <ngyekta@gmail.com>
2026-08-27 19:36:34 +0300

    web: ChartWrapper bg-background

--- apps/web/src/components/metrics/chart-wrapper.tsx
+++ apps/web/src/components/metrics/chart-wrapper.tsx
@@ -12,7 +12,7 @@ type TProps = {
 export default function ChartWrapper({ title, description, className, children }: TProps) {
   return (
     <div className={cn("flex w-full p-1", className)}>
-      <Card className="flex w-full flex-col justify-start rounded-xl border shadow-none">
+      <Card className="bg-background flex w-full flex-col justify-start rounded-xl border shadow-none">
         <CardHeader className="px-4 py-2.25 sm:px-5 sm:py-3">
           <CardTitle className="text-lg">{title}</CardTitle>
           <CardDescription>{description}</CardDescription>
